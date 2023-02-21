cf7f17a399a3fac04627af39167c20da54d394b7
ee-aaron <aaron.horvath@emergence-engineering.com>
2023-02-21 13:34:51 +0100

    adding animation on background in Card

--- src/design/relay/Card.tsx
+++ src/design/relay/Card.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent, ReactNode, useState } from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import Image from "next/image";
 
 export * from "./RobotIcons";
 
@@ -12,7 +13,6 @@ const Root = styled(motion.div)<{ isMobile?: boolean }>`
   min-width: ${(p) => (p.isMobile ? "156px" : "330px")};
   height: ${(p) => (p.isMobile ? "152px" : "330px")};
   cursor: pointer;
-  background-color: grey;
   border-radius: ${(p) => (p.isMobile ? "8px" : "16px")};
   box-shadow: 0 1px 3px rgba(16, 24, 40, 0.1), 0 1px 2px rgba(16, 24, 40, 0.06);
   position: relative;
@@ -43,23 +43,71 @@ export const Card: FunctionComponent<CardProps> = ({
       onClick={handleClick}
       onMouseOver={() => setIsOpen(true)}
       onMouseLeave={() => setIsOpen(false)}
-      initial={{ backgroundColor: initialBgColor }}
+      initial={{
+        background: `radial-gradient(circle at center,${initialBgColor} 0%,${initialBgColor} 50%,${animateBgColor} 51%)`,
+      }}
       animate={
-        isOpen
+        !isOpen
           ? {
-              backgroundColor: initialBgColor,
+              background: `radial-gradient(circle at center,${initialBgColor} 0%,${initialBgColor} 50%,${animateBgColor} 51%)`,
             }
-          : { backgroundColor: animateBgColor }
+          : {
+              background: `radial-gradient(circle at center,${initialBgColor} 0%,${initialBgColor} 99%,${animateBgColor} 100%)`,
+            }
       }
       isMobile={isMobile}
     >
-      {icon && icon}
+      {icon && (
+        <Wrapper>
+          <IconWrapper
+            initial={{ top: "72px" }}
+            animate={
+              isOpen
+                ? {
+                    top: "46px",
+                  }
+                : { top: "72px" }
+            }
+          >
+            {icon}
+          </IconWrapper>
+          <Text
+            initial={{ opacity: "0%" }}
+            animate={
+              isOpen
+                ? {
+                    opacity: "100%",
+                  }
+                : { opacity: "0%" }
+            }
+          >
+            Message the
+            <Image
+              src={"/robot.jpeg"}
+              alt={"robotHead"}
+              width={16}
+              height={16}
+            />
+            <br />
+            for Alchemy
+          </Text>
+        </Wrapper>
+      )}
       {/*<Icon />*/}
     </Root>
   );
 };
 
+const IconWrapper = styled(motion.div)`
+  position: relative;
+`;
 const Wrapper = styled.div`
-  height: 81px;
-  width: 81px;
+  height: 100%;
+`;
+
+const Text = styled(motion.div)`
+  position: absolute;
+  bottom: 2rem;
+  left: 124px;
+  text-align: center;
 `;
